1f269d6016b58252b556f03333454af78078cb63
Anton Shkurenko <tonyshkurenko@gmail.com>
2017-01-24 23:00:38 +0200

    Add game of life renderer

---
+++ GameOfLifeBot/gameoflife_renderer.py
@@ -0,0 +1,61 @@
+from PIL import Image, ImageDraw
+
+# Size
+
+LIFE_WIDTH_IN_CELLS = 20
+LIFE_HEIGHT_IN_CELLS = 20
+
+BORDER_WIDTH = 1
+CELL_SIZE = 7
+
+CELL_PLUS_BORDER = CELL_SIZE + BORDER_WIDTH
+
+IMAGE_WIDTH = LIFE_WIDTH_IN_CELLS * CELL_SIZE + BORDER_WIDTH * (LIFE_WIDTH_IN_CELLS + 1)
+IMAGE_HEIGHT = LIFE_HEIGHT_IN_CELLS * CELL_SIZE + BORDER_WIDTH * (LIFE_HEIGHT_IN_CELLS + 1)
+
+
+# Colors
+
+def color(r=0, g=0, b=0):
+    return int('%02x%02x%02x' % (r, g, b), 16)
+
+
+COLOR_EMPTY = color(255, 255, 255)
+COLOR_FILLED = color()
+COLOR_BORDER = color(100, 100, 100)
+
+
+def draw_grid(canvas, size):
+    # draw vertical lines
+    for x in range(0, size[0], CELL_SIZE + 1):
+        canvas.line((x, 0, x, size[1]), fill=COLOR_BORDER, width=BORDER_WIDTH)
+
+    # draw horizontal lines
+    for y in range(0, size[1], CELL_SIZE + 1):
+        canvas.line((0, y, size[0], y), fill=COLOR_BORDER, width=BORDER_WIDTH)
+
+
+def draw(canvas, life_state):
+    for i in range(0, len(life_state)):
+        for j in range(0, len(life_state[i])):
+            if life_state[i][j] == 0:
+                pass
+            else:
+                x = i * CELL_PLUS_BORDER
+                y = j * CELL_PLUS_BORDER
+                canvas.rectangle((x + BORDER_WIDTH, y + BORDER_WIDTH, x + CELL_SIZE, y + CELL_SIZE), fill=COLOR_FILLED)
+
+
+def main():
+    im = Image.new(mode='RGB', size=(IMAGE_WIDTH, IMAGE_HEIGHT), color=COLOR_EMPTY)
+
+    canvas = ImageDraw.Draw(im)
+
+    draw_grid(canvas, im.size)
+    draw(canvas, [[1, 0, 1], [0, 1, 0], [1, 0, 1]])
+
+    im.save("temp.png", "PNG")
+
+
+if __name__ == '__main__':
+    main()
